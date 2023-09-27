6f97be930bd0ccab44af3dc213c6c8da7c99b27f
olfa wiersinga <wiersingaolfa@yahoo.fr>
2023-09-27 17:00:40 +0200

    create a Vuejs project,add Component CitiesList.vue, install the vue-router

---
+++ meteo-project/views/CitiesList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="titleListCities">
+    <h1>{{msg}}</h1>
+    <img src="https://www.rts.ch/2020/05/22/14/20/9390881.image?w=1200&h=630">
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Météo - listes des villes',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: rebeccapurple;
+}
+
+</style>
